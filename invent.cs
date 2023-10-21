1c844f6d5eb54fffe8c37dfafcbc7878daa03d19
KANISHK SANCHIHAR <146233836+Kanishk-Sanchihar@users.noreply.github.com>
2023-10-21 23:14:23 +0530

    Add files via upload

---
+++ invent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+public class invent
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+
+	public invent()
+	{
+        con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=Inventry;Integrated Security=True");
+        con.Open();
+
+	}
+    public void command(String s)
+    {
+        cmd = new SqlCommand(s, con);
+        cmd.ExecuteNonQuery();
+    }
+
+    public DataSet select(String s)
+    {
+        DataSet ds = new DataSet();
+        da = new SqlDataAdapter(s, con);
+        da.Fill(ds);
+        return (ds);
+    }
+}
